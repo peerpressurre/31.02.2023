e73a54f6f6c8f3113abdfbcc34084ea18c763a5a
peerpressurre <gsophiay@gmail.com>
2023-01-30 20:19:04 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdlib.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+	char sym;
+	int way, speed;
+	cout << "Enter symbol-> ";
+	cin >> sym;
+	cout << "Enter [ 1 ] for horizontal output or [ 2 ] for vertical-> ";
+	cin >> way;
+	cout << "Choose speed of output: [ 1] for Fast || [ 2 ] for Normal || [ 3 ] for Slow -> ";
+	cin >> speed;
+	switch (way)
+	{
+	case 1:
+	{
+		for (size_t i = 0; i < 10 + 1; i++)
+		{
+			cout << sym << " ";
+			if (speed == 1)
+			{
+				Sleep(100);
+			}
+			else if (speed == 2)
+			{
+				Sleep(500);
+			}
+			else if (speed == 3)
+			{
+				Sleep(1000);
+			}
+
+		}
+	}
+	default:
+		break;
+	}
+	
+
+
+	return 0;
+}
+
+	//if (speed == 1)
+	//{
+	//	for (size_t i = 0; i < 10 + 1; i++)
+	//	{
+	//		cout << sym << " ";
+	//		if (true)
+	//		{
+
+	//		}
+	//		Sleep(100);
+	//	}
+
+	//}
